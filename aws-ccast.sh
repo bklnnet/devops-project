ae397a607e6594b8a9957eb9a011d61d544f1425
Mark Naumowicz <bklnnet@gmail.com>
2017-07-21 00:40:47 -0400

    Create aws-ccast.sh

---
+++ aws-ccast.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+mkdir HelloWorld
+cd HelloWorld
+eb init -p PHP
+eb create dev-env
+openssl genrsa -des3 -out server.key 1024
+openssl req -new -key server.key -out server.csr
+cp server.key server.key.org
+openssl rsa -in server.key.org -out server.key
+openssl x509 -req -days 365 -in server.csr -signkey server.key -out server.crt
