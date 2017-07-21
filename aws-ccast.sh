1844c3f759fb5b4dfd9a8822770180f320df3d2d
Mark Naumowicz <bklnnet@gmail.com>
2017-07-21 01:04:06 -0400

    Update aws-ccast.sh

--- aws-ccast.sh
+++ aws-ccast.sh
@@ -10,3 +10,5 @@ openssl req -new -key server.key -out server.csr
 cp server.key server.key.org
 openssl rsa -in server.key.org -out server.key
 openssl x509 -req -days 365 -in server.csr -signkey server.key -out server.crt
+aws iam upload-server-certificate --server-certificate-name JollyBuyInc --certificate-body file://server.crt --private-key file://server.key
+
